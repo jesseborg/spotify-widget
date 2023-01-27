1049d35b18f41479713631f637d47bb8d83ce801
xJeex <6882782+xjeex@users.noreply.github.com>
2023-01-27 14:08:29 +1000

    separate timeline and slider position

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,11 +1,11 @@
-import { FC, HTMLAttributes, useEffect, useState } from 'react';
+import { FC, HTMLAttributes, useState } from 'react';
 import { MediaTimelineData } from '../utils/bindings';
 import { clsx } from '../utils/clsx';
 import { formatTime } from '../utils/time';
 import { PropsWithLoading } from '../utils/types';
 import { Slider } from './base/Slider';
 
-type Props = {
+type TimelineProps = {
 	data?: MediaTimelineData | null;
 	isPlaying?: boolean;
 };
@@ -33,12 +33,12 @@ const FormattedTime: FC<FormattedTimeProps & HTMLAttributes<HTMLParagraphElement
 	);
 };
 
-export const Timeline: FC<PropsWithLoading<Props>> = ({ data, isPlaying, loading }) => {
-	const [timelinePosition, setTimelinePosition] = useState(
-		Number(data?.timelinePosition ?? 0) / 10000
-	);
+export const Timeline: FC<PropsWithLoading<TimelineProps>> = ({ data, isPlaying, loading }) => {
+	const timelinePosition = Number(data?.timelinePosition ?? 0) / 10000;
 	const timelineEndTime = Number(data?.timelineEndTime ?? 0) / 10000;
-	const timelinePositionPercent = (timelinePosition / timelineEndTime) * 100;
+
+	const [sliderPosition, setSliderPosition] = useState(timelinePosition);
+	const timelinePositionPercent = (sliderPosition / timelineEndTime) * 100;
 
 	const [isDragging, setIsDragging] = useState(false);
 
@@ -46,56 +46,13 @@ export const Timeline: FC<PropsWithLoading<Props>> = ({ data, isPlaying, loading
 		setIsDragging(true);
 		// console.log(value);
 
-		setTimelinePosition(value[0]);
+		setSliderPosition(value[0]);
 	};
 
 	const handleValueCommit = (value: number[]) => {
 		setIsDragging(false);
 		// emit event to backend
 	};
-
-	useEffect(() => {
-		if (isDragging) {
-			return;
-		}
-
-		setTimelinePosition(Number(data?.timelinePosition ?? 0) / 10000);
-	}, [data?.timelinePosition]);
-
-	// const [timelinePosition, setTimelinePosition] = useState(
-	// 	Number(data?.timelinePosition ?? 0) / 10000
-	// );
-
-	// useEffect(() => {
-	// 	let currentPosition = Number(data?.timelinePosition ?? 0) / 10000;
-	// 	let { ms } = getTimeData(currentPosition);
-
-	// 	const updateTime = (diff: number = 0) => {
-	// 		if (!isPlaying || Number(data?.timelineEndTime) === 0) {
-	// 			setTimelinePosition(currentPosition);
-	// 			return;
-	// 		}
-
-	// 		if (diff) {
-	// 			console.log(diff, 1000 - diff);
-	// 		}
-
-	// 		setTimelinePosition((currentPosition += 1000 - diff));
-	// 	};
-
-	// 	// Round up to the next second
-	// 	updateTime(1000 - ms);
-	// 	const timer = setInterval(updateTime, 1000);
-
-	// 	if (!isPlaying || Number(data?.timelineEndTime) === 0) {
-	// 		clearInterval(timer);
-	// 		return;
-	// 	}
-
-	// 	return () => {
-	// 		clearInterval(timer);
-	// 	};
-	// }, [data, isPlaying]);
 
 	if (loading) {
 		return (
@@ -117,20 +74,18 @@ export const Timeline: FC<PropsWithLoading<Props>> = ({ data, isPlaying, loading
 				/>
 			</div>
 			<span className="flex h-1 items-center">
-				{isDragging && (
-					<FormattedTime
-						style={{
-							left: `calc(${timelinePositionPercent}%)`,
-							transform: `translateX(-${timelinePositionPercent}%)`
-						}}
-						className={clsx('absolute -top-1/2 transition-all', {
-							'transition-none': isDragging
-						})}
-						value={timelinePosition}
-					/>
-				)}
+				<FormattedTime
+					style={{
+						left: `${timelinePositionPercent}%`,
+						transform: `translateX(-${timelinePositionPercent}%)`
+					}}
+					className={clsx('absolute -top-1/2 opacity-0 transition-all', {
+						'opacity-100 transition-none': isDragging
+					})}
+					value={sliderPosition}
+				/>
 				<Slider
-					value={[timelinePosition]}
+					value={[isDragging ? sliderPosition : timelinePosition]}
 					max={timelineEndTime}
 					onValueChange={handleValueChange}
 					onValueCommit={handleValueCommit}
